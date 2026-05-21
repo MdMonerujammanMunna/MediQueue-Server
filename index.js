6904bd554dac8aa594b78a9bb8361dfbce2dadc7
Md Monerujamman Munna <mdmonerujammanmunna@gmail.com>
2026-05-21 17:03:30 +0600

    add delete and update api

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const cors = require("cors")
 const dotenv = require("dotenv")
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { useId } = require("react");
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
+const { verify } = require("node:crypto");
 const app = express()
 dotenv.config()
 const PORT = process.env.PORT
@@ -26,9 +28,31 @@ const client = new MongoClient(uri, {
     }
 });
 
+const JWKS = createRemoteJWKSet(
+    new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
+)
+
+const Varify = async (req, res, next) => {
+    const AuthValu = req?.headers.authorization;
+    if (!AuthValu) {
+        return res.status(401).json({ message: "Unauthorization" })
+    }
+    const Token = AuthValu.split(" ")[1]
+    if (!Token) {
+        return res.status(401).json({ message: "Unauthorization" })
+    }
+    try {
+        const { payload } = await jwtVerify(Token, JWKS)
+        next()
+    } catch (error) {
+        return res.status(403).json({ message: "Forbidden" })
+    }
+
+}
+
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
 
 
         const db = client.db("MediQueue")
@@ -41,18 +65,18 @@ async function run() {
             const result = await dataCollection.insertOne(Data)
             res.json(result)
         })
-        // All data
+        // All data done
         app.get("/Tutors", async (req, res) => {
             const result = await dataCollection.find().toArray()
             res.json(result)
         })
-        // Details page
+        // Details page done
         app.get("/AllTutorPage/:id", async (req, res) => {
             const { id } = req.params
             const result = await dataCollection.findOne({ _id: new ObjectId(id) })
             res.json(result)
         })
-        // All data
+        // All data done
         app.get("/TutorsLimit", async (req, res) => {
             const result = await dataCollection.find().limit(6).toArray()
             res.json(result)
@@ -63,22 +87,35 @@ async function run() {
             const result = await bookingCollection.insertOne(Data)
             res.json(result)
         })
-        // Booking data
+        // Booking data done
         app.get("/Bookingall", async (req, res) => {
             const Data = req.body
             const result = await bookingCollection.find().toArray()
             res.json(result)
         })
 
-        // My maked
+        // My maked done
         app.get("/myMaked", async (req, res) => {
             const userId = req.headers.uservalidid || req.headers['uservalidid'];
-
             const result = await dataCollection.find({ SessionUserID: userId }).toArray()
+            res.json(result)
+        })
+        // Update done
+        app.patch("/Tutors/:id", async (req, res) => {
+            const { id } = req.params
+            const updateData = req.body
+            const result = await dataCollection.updateOne({ _id: new ObjectId(id) }, { $set: updateData })
             res.json(result)
             console.log(result)
         })
-        await client.db("admin").command({ ping: 1 });
+        // Delect 
+        app.delete("/Tutors/:id", async (req, res) => {
+            const { id } = req.params
+            const result = await dataCollection.deleteOne({ _id: new ObjectId(id) })
+            res.json(result)
+            console.log(result)
+        })
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // await client.close();
